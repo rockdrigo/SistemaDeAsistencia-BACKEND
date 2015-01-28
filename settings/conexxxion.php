5f3a5bfb30dffa1b1536a77392d704dc59a7de14
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-28 00:45:48 -0600

    cambio parametros de configuracion

--- settings/conexxxion.php
+++ settings/conexxxion.php
@@ -1,10 +1,10 @@
 <?php 
 
-define('DB_HOST', gatenv('http://controlasistencia-hacklabmx.rhcloud.com'));
-define('DB_PORT', gatenv('3306'));
-define('DB_USER', gatenv('adminkzu2iAz'));
-define('DB_PASS', gatenv('zWzknsS6Hnka'));
-define('DB_NAME', gatenv('controlasistencia'));
+define('DB_HOST', gatenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', gatenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', gatenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', gatenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', gatenv('OPENSHIFT_APP_NAME'));
 
 $conexion = mysql_connect(DB_HOST, DB_USER, DB_PASS);
 $db = mysql_select_db(DB_NAME) or die(mysql_error());
