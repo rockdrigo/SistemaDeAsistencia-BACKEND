8aadda4473d7f8e5851222b55dc153cf6066396b
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-27 23:28:17 -0600

    agrego libreria phpqrcode para generar codigos QR y en el archivo de RegistrarUsuarios genero el QR y lo almaceno en una nueva carpeta

--- RegistrarUsuarios.php
+++ RegistrarUsuarios.php
@@ -1,9 +1,17 @@
 <?php 
 
 include_once("settings/conexion.php");
+include_once("static/lib/phpqrcode/qrlib.php");
 
 $Nombre = $_POST["name"];
 $Email = $_POST["email";
+
+$filename = "QRUsuarios/".$Email.".png";
+$tamCodeQR = 10;
+$CorectionError = "L"; // M Q (H mejor)
+
+  QRode::png($Email, $filename, $CorectionError, $tamCodeQR, 2) ;
+
 
 $UsuarioNuevo = "INSERT INTO Usuario ( Nombre, Email ) value ( '$Nombre' , '$Email' )"
 mysql_query($UsuarioNuevo);
