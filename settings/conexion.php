0822d10fc5cb3b1dc1ae3ea1564b1d74b6e072d8
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-28 01:55:22 -0600

    ahi va todo6

--- settings/conexion.php
+++ settings/conexion.php
@@ -6,7 +6,7 @@
   define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
   define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
-  $conexion = mysql_connect("$DB_HOST", "$DB_USER", "$DB_PASS", "$DB_NAME");
+  $conexion = mysql_connect("$DB_HOST", "$DB_USER", "$DB_PASS");
   $db = mysql_select_db(DB_NAME) or die(mysql_error());
 
 ?>
