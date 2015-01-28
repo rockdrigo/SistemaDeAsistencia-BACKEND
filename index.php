60ea67f96cd30db4d7a88ffbe6002edf113191c4
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-27 22:34:36 -0600

    cambio de versiones

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <title>Control de Asistencia - HackLab Eventos</title>
-  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" type="text/css">
-  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" type="text/css">
   <!-- tema del sitio -->
   <link rel="stylesheet" href="static/css/AdminLTE.css" type="text/css">
 </head>
@@ -22,11 +22,11 @@
         </div>
       </div>
       <div class="footer">
-        <button id="Btnregistrar" type="submit" class="btn bg-olive btn-block">Enviar</button>
+        <button id="BtnRegistrar" type="submit" class="btn bg-olive btn-block">Enviar</button>
       </div>
     </form>
   </div>
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
