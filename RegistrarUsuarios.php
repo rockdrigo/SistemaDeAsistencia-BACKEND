0507bb30c0135631b7cda553b68e8029256b0a34
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-28 00:56:46 -0600

    cambio tonterias que podria hacer que funciene el programa

--- RegistrarUsuarios.php
+++ RegistrarUsuarios.php
@@ -1,19 +1,19 @@
 <?php 
 
-include_once("settings/conexion.php");
-include_once("static/lib/phpqrcode/qrlib.php");
+include_once('settings/conexion.php');
+include_once('static/lib/phpqrcode/qrlib.php');
 
-$Nombre = $_POST["name"];
-$Email = $_POST["email";
+$Nombre = $_POST['name'];
+$Email = $_POST['email';
 
 $filename = "QRUsuarios/".$Email.".png";
 $tamCodeQR = 10;
 $CorectionError = "L"; // M Q (H mejor)
 
-  QRode::png($Email, $filename, $CorectionError, $tamCodeQR, 2) ;
+  QRode::png($Email, $filename, $CorectionError, $tamCodeQR, 2);
 
 
-$UsuarioNuevo = "INSERT INTO Usuario ( Nombre, Email ) value ( '$Nombre' , '$Email' )"
+$UsuarioNuevo = "INSERT INTO Usuario ( Nombre, Email ) value ( '$Nombre' , '$Email' )";
 mysql_query($UsuarioNuevo);
 
 header("Location:UsuarioRegistrado.php?email=".$Email."&nombre".$Nombre);
