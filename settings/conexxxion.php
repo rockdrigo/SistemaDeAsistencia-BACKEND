3a382037bd42f736c4e022fa05a4d5b349c63fe8
Rodrigo Satch <roo@ciencias.unam.mx>
2015-01-28 00:48:34 -0600

    cambio parametros de configuracion

--- settings/conexxxion.php
+++ settings/conexxxion.php
@@ -1,10 +1,10 @@
 <?php 
 
-define('DB_HOST', gatenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', gatenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', gatenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', gatenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', gatenv('OPENSHIFT_APP_NAME'));
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
 $conexion = mysql_connect(DB_HOST, DB_USER, DB_PASS);
 $db = mysql_select_db(DB_NAME) or die(mysql_error());
